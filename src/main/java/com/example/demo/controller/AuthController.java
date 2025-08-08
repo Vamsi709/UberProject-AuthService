ac5d76ce54d4a78070bc2d6888cff28d328eac4c
coderVamsi <vamsiganta909@gmail.com>
2025-08-08 15:52:45 +0530

    Adding changes

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     @PostMapping("/signup/passenger")
-    public ResponseEntity<?> signUp(@RequestBody PassengerSignUpRequestDto){
+    public ResponseEntity<?> signUp(@RequestBody PassengerSignUpRequestDto passengerSignUpRequestDto){
         return null;
 
     }
